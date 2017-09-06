3c01f39b96366d0177bf2c86e6381f425756a200
Venkata Satyanarayana. Alokam <31393949+alokamvenki402@users.noreply.github.com>
2017-09-06 09:14:12 -0400

    only calender

--- date_time.py
+++ date_time.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python
- 
-import time
-print (time.strftime("%H:%M:%S"))
- 
-## 12 hour format ##
-print (time.strftime("%I:%M:%S"))
+import calendar
+cal = calendar.LocaleTextCalendar(locale='en_AU.utf8')
+print(cal.prmonth(2025, 9))
